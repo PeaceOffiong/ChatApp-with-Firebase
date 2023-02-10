39f7fc3e81fcd49cbbcef4309a2339864dc4e3ce
peaceOffiong <peaceyben@gamil.com>
2023-02-10 06:32:14 -0800

    final commit, bug fixed

--- src/db.js
+++ src/db.js
@@ -1,10 +1,17 @@
-import firebase from "firebase/app";
-import "firebase/database";
+import firebase from "firebase/compat/app";
+import "firebase/compat/database"
 
 const config = {
-
-}
+  apiKey: "AIzaSyCSROgkj5OgqNMYSmFfjjRvl0egAkRwp4c",
+  authDomain: "fir-chatapp-b04b9.firebaseapp.com",
+  databaseURL: "https://fir-chatapp-b04b9-default-rtdb.firebaseio.com",
+  projectId: "fir-chatapp-b04b9",
+  storageBucket: "fir-chatapp-b04b9.appspot.com",
+  messagingSenderId: "381190491416",
+  appId: "1:381190491416:web:02dd64e5f05321af99226d",
+};
 
 const db = firebase.initializeApp(config);
-
 export default db;
+
+
--- src/views/HomeView.vue
+++
@@ -1,9 +0,0 @@
-<script setup>
-
-</script>
-
-<template>
-  <main>
-   <h2>Hello</h2>
-  </main>
-</template>
